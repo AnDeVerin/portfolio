15e534f4f4809bf3921311684ffff0e53da2f8b3
AnDeVerin <adw006@ya.ru>
2017-12-15 08:35:44 +0300

    fixed welcome screen video background

--- source/js/app.js
+++ source/js/app.js
@@ -1,20 +1,39 @@
 'use strict';
 
-//--- paralax effect on Welcome page------------------------------------------
-const welcome = document.querySelector('#welcome');
-const bgContainer = document.querySelector('#welcome-bg');
-const moveFactor = 0.01;
+document.addEventListener('DOMContentLoaded', ready);
 
-function moveBackground(e) {
-  let initialX = (welcome.clientWidth / 2) - e.pageX;
-  let initialY = (welcome.clientHeight / 2) - e.pageY;
-  let positionX = initialX * moveFactor;
-  let positionY = initialY * moveFactor;
+function ready() {
 
-  bgContainer.style.transform = `translate( ${ positionX }px, 
+  const welcome = document.querySelector('#welcome');
+  const bgContainer = document.querySelector('#welcome-bg');
+  const moveFactor = 0.01;
+
+  let animationDisable = false;
+
+  //--- if table or phone, remove video and parallax blocks
+  if (screen.width <= '992') {
+    animationDisable = true;
+    welcome.querySelector('video').remove();
+    welcome.querySelector('#welcome-bg').remove();
+  }
+
+  //--- paralax effect on Welcome page------------------------------------------
+
+
+  function moveBackground(e) {
+    let initialX = (welcome.clientWidth / 2) - e.pageX;
+    let initialY = (welcome.clientHeight / 2) - e.pageY;
+    let positionX = initialX * moveFactor;
+    let positionY = initialY * moveFactor;
+
+    bgContainer.style.transform = `translate( ${ positionX }px, 
                                             ${ positionY }px)`;
-}
+  }
 
-welcome.addEventListener('mousemove', moveBackground);
+  if (!animationDisable) {
+    welcome.addEventListener('mousemove', moveBackground);
+  }
 
 //----------------------------------------------------------------------------
+}
+
