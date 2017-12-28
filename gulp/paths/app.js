0020b82f41b110a749110d1ab57eb4113072632d
AnDeVerin <adw006@ya.ru>
2017-12-28 17:17:13 +0300

    added all js-files to app.js

--- gulp/paths/app.js
+++ gulp/paths/app.js
@@ -1,14 +1,5 @@
 'use strict';
 
 module.exports = [
-  './source/js/app.js',
-  './source/js/preloader.js',
-  './source/js/common.js',
-  './source/js/slider.js',
-  './source/js/index-page.js',
-  './source/js/about-page.js',
-  './source/js/blog-page.js',
-  './source/js/works-page.js',
-
-  './source/js/init-map.js'
+  './source/js/app.js'
 ];
