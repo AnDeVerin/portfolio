d14c9554b91c822a544b9a8c4dabdd9f18d78f57
AnDeVerin <adw006@ya.ru>
2018-01-07 17:16:34 +0300

    some fix

--- app.js
+++ app.js
@@ -92,8 +92,8 @@ app.use(function(err, req, res, next) {
   res.render('500');
 });
 
-//server.listen(3000, 'localhost');
-server.listen(80, '0.0.0.0');
+server.listen(3000, 'localhost');
+//server.listen(3000, '0.0.0.0');
 server.on('listening', function() {
   jsonfile.readFile(fileVersionControl, function(err, obj) {
     if (err) {
